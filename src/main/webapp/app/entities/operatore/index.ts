17c311d4eb49e8b244154e289157bf9a1cce466d
DIEGO VALERIO <valerio.diego@cnr.it>
2018-12-21 10:50:54 +0100

    rimosso tutto

--- src/main/webapp/app/entities/operatore/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './operatore.service';
-export * from './operatore-update.component';
-export * from './operatore-delete-dialog.component';
-export * from './operatore-detail.component';
-export * from './operatore.component';
-export * from './operatore.route';
--- src/main/webapp/app/entities/telefono/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './telefono.service';
-export * from './telefono-update.component';
-export * from './telefono-delete-dialog.component';
-export * from './telefono-detail.component';
-export * from './telefono.component';
-export * from './telefono.route';
