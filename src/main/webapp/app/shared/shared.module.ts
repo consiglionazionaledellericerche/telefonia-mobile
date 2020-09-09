f31156fe82486b4d6a54a183bafee21cd22f40db
valerio.diego <valerio.diego@cnr.it>
2020-09-09 15:33:03 +0200

    Inserito date in formato dd/mm/YYYY

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -1,13 +1,24 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateAdapter, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 
 import { NgbDateMomentAdapter } from './util/datepicker-adapter';
+import { NgbDateCustomParserFormatter } from './util/datecustom-adapter';
 import { TelefoniaSharedLibsModule, TelefoniaSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective } from './';
 
 @NgModule({
     imports: [TelefoniaSharedLibsModule, TelefoniaSharedCommonModule],
     declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective],
-    providers: [{ provide: NgbDateAdapter, useClass: NgbDateMomentAdapter }],
+    providers: [
+        {
+            provide: NgbDateAdapter,
+            useClass: NgbDateMomentAdapter
+        },
+        {
+            provide: NgbDateParserFormatter,
+            useClass: NgbDateCustomParserFormatter
+        }
+    ],
+
     entryComponents: [JhiLoginModalComponent],
     exports: [TelefoniaSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
