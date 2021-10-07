30edb0a466d63cbfbae4973b6c9566b18b735990
PIERLEONI FABRIZIO <fabrizio.pierleoni@cnr.it>
2021-10-07 16:31:10 +0200

    revert

--- src/main/java/it/cnr/si/service/UserService.java
+++ src/main/java/it/cnr/si/service/UserService.java
@@ -1,28 +1,18 @@
 package it.cnr.si.service;
 
-import feign.FeignException;
 import it.cnr.si.config.Constants;
 import it.cnr.si.domain.Authority;
 import it.cnr.si.domain.User;
 import it.cnr.si.repository.AuthorityRepository;
 import it.cnr.si.repository.UserRepository;
-import it.cnr.si.security.ACEAuthentication;
 import it.cnr.si.security.AuthoritiesConstants;
 import it.cnr.si.security.SecurityUtils;
 import it.cnr.si.service.dto.UserDTO;
 
-import it.cnr.si.service.dto.anagrafica.enums.TipoAppartenenza;
-import it.cnr.si.service.dto.anagrafica.enums.TipoRuolo;
-import it.cnr.si.service.dto.anagrafica.scritture.BossDto;
-import it.cnr.si.service.dto.anagrafica.simpleweb.SimpleEntitaOrganizzativaWebDto;
-import it.cnr.si.service.dto.anagrafica.simpleweb.SimpleUtenteWebDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -32,7 +22,7 @@ import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
+import java.time.Instant;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -50,15 +40,9 @@ public class UserService {
 
     private final AuthorityRepository authorityRepository;
 
-    private final AceService aceService;
-
-    @Value("#{'${ace.contesto}'.split(',')}")
-    private List<String> contestoACE;
-
-    public UserService(UserRepository userRepository, AuthorityRepository authorityRepository, AceService aceService) {
+    public UserService(UserRepository userRepository, AuthorityRepository authorityRepository) {
         this.userRepository = userRepository;
         this.authorityRepository = authorityRepository;
-        this.aceService = aceService;
     }
 
     /**
@@ -126,44 +110,22 @@ public class UserService {
      */
     @SuppressWarnings("unchecked")
     public UserDTO getUserFromAuthentication(OAuth2Authentication authentication) {
+        Object oauth2AuthenticationDetails = authentication.getDetails(); // should be an OAuth2AuthenticationDetails
         Map<String, Object> details = (Map<String, Object>) authentication.getUserAuthentication().getDetails();
+        User user = getUser(details);
+        Set<Authority> userAuthorities = extractAuthorities(authentication, details);
+        Authority roleUser = new Authority();
+        roleUser.setName(AuthoritiesConstants.USER);
+        userAuthorities.add(roleUser);
+        user.setAuthorities(userAuthorities);
 
         // convert Authorities to GrantedAuthorities
         Set<GrantedAuthority> grantedAuthorities = getRoles(details).stream()
             .map(SimpleGrantedAuthority::new)
             .collect(Collectors.toSet());
-
-        User user = getUser(details);
-        user.setAuthorities(
-            grantedAuthorities
-                .stream()
-                .map(a -> {
-                    Authority auth = new Authority();
-                    auth.setName(a.getAuthority());
-                    return auth;
-                })
-                .collect(Collectors.toSet())
-        );
-
-        Set<Authority> userAuthorities = new HashSet<>();
-        for(GrantedAuthority authority: grantedAuthorities){
-            Authority auth = new Authority();
-            auth.setName(authority.getAuthority());
-            userAuthorities.add(auth);
-        }
-        user.setAuthorities(userAuthorities);
+        grantedAuthorities.add(new SimpleGrantedAuthority(AuthoritiesConstants.USER));
 
         UsernamePasswordAuthenticationToken token = getToken(details, user, grantedAuthorities);
-
-        // workaround... @marco.pasiano: fix please !
-        Authority auth = null;
-        for(GrantedAuthority ga : token.getAuthorities()){
-            auth = new Authority();
-            auth.setName(ga.getAuthority());
-            user.getAuthorities().add(auth);
-        }
-
-        Object oauth2AuthenticationDetails = authentication.getDetails(); // should be an OAuth2AuthenticationDetails
         authentication = new OAuth2Authentication(authentication.getOAuth2Request(), token);
         authentication.setDetails(oauth2AuthenticationDetails); // must be present in a gateway for TokenRelayFilter to work
         SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -171,93 +133,84 @@ public class UserService {
         return new UserDTO(user);
     }
 
-    private  UsernamePasswordAuthenticationToken getToken(Map<String, Object> details, User user, Set<GrantedAuthority> grantedAuthorities) {
+    private User syncUserWithIdP(Map<String, Object> details, User user) {
+        // save authorities in to sync user roles/groups between IdP and JHipster's local database
+        Collection<String> dbAuthorities = getAuthorities();
+        Collection<String> userAuthorities =
+            user.getAuthorities().stream().map(Authority::getName).collect(Collectors.toList());
+        for (String authority : userAuthorities) {
+            if (!dbAuthorities.contains(authority)) {
+                log.debug("Saving authority '{}' in local database", authority);
+                Authority authoritytoSave = new Authority();
+                authoritytoSave.setName(authority);
+                authorityRepository.save(authoritytoSave);
+            }
+        }
+        // save account in to sync users between IdP and JHipster's local database
+        Optional<User> existingUser = userRepository.findOneByLogin(user.getLogin());
+        if (existingUser.isPresent()) {
+            // if IdP sends last updated information, use it to determine if an update should happen
+            if (details.get("updated_at") != null) {
+                Instant dbModifiedDate = existingUser.get().getLastModifiedDate();
+                Instant idpModifiedDate = new Date(Long.valueOf((Integer) details.get("updated_at"))).toInstant();
+                if (idpModifiedDate.isAfter(dbModifiedDate)) {
+                    log.debug("Updating user '{}' in local database", user.getLogin());
+                    updateUser(user.getFirstName(), user.getLastName(), user.getEmail(),
+                        user.getLangKey(), user.getImageUrl());
+                }
+                // no last updated info, blindly update
+            } else {
+                log.debug("Updating user '{}' in local database", user.getLogin());
+                updateUser(user.getFirstName(), user.getLastName(), user.getEmail(),
+                    user.getLangKey(), user.getImageUrl());
+            }
+        } else {
+            log.debug("Saving user '{}' in local database", user.getLogin());
+            userRepository.save(user);
+        }
+        return user;
+    }
+
+    private static UsernamePasswordAuthenticationToken getToken(Map<String, Object> details, User user, Set<GrantedAuthority> grantedAuthorities) {
         // create UserDetails so #{principal.username} works
-        UserDetails utente =
+        UserDetails userDetails =
             new org.springframework.security.core.userdetails.User(user.getLogin(),
                 "N/A", grantedAuthorities);
-
-        String principal = Optional.ofNullable(user.getLogin())
-            .filter(String.class::isInstance)
-            .map(String.class::cast)
-            .map(String::toLowerCase)
-            .orElseThrow(() -> new BadCredentialsException("login.messages.error.authentication"));
-
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        List<BossDto> bossDtos = aceService.ruoliUtenteAttivi(principal);
-        authorities.addAll(
-            bossDtos.stream()
-                .filter(bossDto -> contestoACE.contains(bossDto.getRuolo().getContesto().getSigla()))
-                .filter(bossDto -> {
-                    return !(bossDto.getEntitaOrganizzativa() != null && bossDto.getRuolo().getTipoRuolo().equals(TipoRuolo.ROLE_ADMIN));
-                })
-                .map(a -> new SimpleGrantedAuthority(
-                    Optional.ofNullable(a.getRuolo().getTipoRuolo()).map(TipoRuolo::name).orElse(AuthoritiesConstants.USER))
-                )
-                .distinct()
-                .collect(Collectors.toList()));
-
-        Stream<SimpleEntitaOrganizzativaWebDto> entitaOrganizzativaAssegnata = bossDtos.stream()
-            .filter(bossDto -> contestoACE.contains(bossDto.getRuolo().getContesto().getSigla()))
-            .filter(bossDto -> Optional.ofNullable(bossDto.getEntitaOrganizzativa()).isPresent())
-            .map(bossDto -> bossDto.getEntitaOrganizzativa());
-
-        if (bossDtos.isEmpty()) {
-            authorities.addAll(
-                aceService.ruoliAttivi(principal).stream()
-                    .filter(ruoloWebDto -> contestoACE.contains(ruoloWebDto.getContesto().getSigla()))
-                    .map(a -> new SimpleGrantedAuthority(
-                        Optional.ofNullable(a.getTipoRuolo()).map(TipoRuolo::name).orElse(AuthoritiesConstants.USER))
-                    )
-                    .distinct()
-                    .collect(Collectors.toList()));
-        }
-        if (authorities.isEmpty()) {
-            // nessuna authorities => utente non abilitato
-        }
-        if (!authorities.contains(new SimpleGrantedAuthority(AuthoritiesConstants.USER))) {
-            authorities.add(new SimpleGrantedAuthority(AuthoritiesConstants.USER));
-        }
-
-        try {
-            SimpleUtenteWebDto utenteWebDto = aceService.getUtente(principal);
-            if (Optional.ofNullable(utenteWebDto.getPersona()).isPresent()) {
-                List<SimpleEntitaOrganizzativaWebDto> entitaOrganizzativeStruttura =
-                    aceService.findEntitaOrganizzativeStruttura(principal, LocalDate.now(), TipoAppartenenza.SEDE);
-                return new ACEAuthentication(utente, utenteWebDto, "N/A", authorities,
-                    Stream.concat(
-                        entitaOrganizzativaAssegnata.map(SimpleEntitaOrganizzativaWebDto::getCdsuo).distinct(),
-                        entitaOrganizzativeStruttura.stream().map(SimpleEntitaOrganizzativaWebDto::getCdsuo).distinct()
-                    ).collect(Collectors.toList())
-                );
-            }
-        } catch (FeignException e) {
-            log.warn("Person not found for principal {}", principal);
-        }
-
         // update Spring Security Authorities to match groups claim from IdP
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
-            utente, "N/A", grantedAuthorities);
+            userDetails, "N/A", grantedAuthorities);
         token.setDetails(details);
-
         return token;
     }
 
+    @SuppressWarnings("unchecked")
+    private static Set<Authority> extractAuthorities(OAuth2Authentication authentication, Map<String, Object> details) {
+        Set<Authority> userAuthorities;
+        // get roles from details
+        if (details.get("roles") != null) {
+            userAuthorities = extractAuthorities((List<String>) details.get("roles"));
+            // if roles don't exist, try groups
+        } else if (details.get("groups") != null) {
+            userAuthorities = extractAuthorities((List<String>) details.get("groups"));
+        } else {
+            userAuthorities = authoritiesFromStringStream(
+                authentication.getAuthorities().stream()
+                    .map(GrantedAuthority::getAuthority)
+            );
+        }
+        // convert Authorities to GrantedAuthorities
+        userAuthorities.addAll(authoritiesFromStringStream(getRoles(details).stream()));
 
+        return userAuthorities;
+    }
 
     private static List<String> getRoles(Map<String, Object> details) {
 
         List list = new ArrayList();
         try {
-            if(details==null){
-                return list;
-            }
-
-            if(details.get("contexts") != null) {
-                Map context = (Map) ((Map) details.get("contexts")).get("telefonia-app");
-                if(context != null) {
-                    list = (List) context.get("roles");
-                }
+            Map context = (Map) ((Map) details.get("contexts")).get("telefonia-app");
+            if(context != null) {
+                list = (List) context.get("roles");
             }
         } catch (Exception e) {
             // TODO: inserire log....
@@ -270,40 +223,50 @@ public class UserService {
     private static User getUser(Map<String, Object> details) {
         User user = new User();
         user.setId(1L);
-
-        if(details != null) {
-            if (details.get("username_cnr") != null) {
-                user.setLogin(((String) details.get("username_cnr")).toLowerCase());
+        user.setLogin(((String) details.get("username_cnr")).toLowerCase());
+        if (details.get("given_name") != null) {
+            user.setFirstName((String) details.get("given_name"));
+        }
+        if (details.get("family_name") != null) {
+            user.setLastName((String) details.get("family_name"));
+        }
+        if (details.get("email_verified") != null) {
+            user.setActivated((Boolean) details.get("email_verified"));
+        }
+        if (details.get("email") != null) {
+            user.setEmail(((String) details.get("email")).toLowerCase());
+        }
+        if (details.get("langKey") != null) {
+            user.setLangKey((String) details.get("langKey"));
+        } else if (details.get("locale") != null) {
+            String locale = (String) details.get("locale");
+            if (locale.contains("-")) {
+                String langKey = locale.substring(0, locale.indexOf("-"));
+                user.setLangKey(langKey);
+            } else if (locale.contains("_")) {
+                String langKey = locale.substring(0, locale.indexOf("_"));
+                user.setLangKey(langKey);
             }
-            if (details.get("given_name") != null) {
-                user.setFirstName((String) details.get("given_name"));
-            }
-            if (details.get("family_name") != null) {
-                user.setLastName((String) details.get("family_name"));
-            }
-            if (details.get("email_verified") != null) {
-                user.setActivated((Boolean) details.get("email_verified"));
-            }
-            if (details.get("email") != null) {
-                user.setEmail(((String) details.get("email")).toLowerCase());
-            }
-            if (details.get("langKey") != null) {
-                user.setLangKey((String) details.get("langKey"));
-            } else if (details.get("locale") != null) {
-                String locale = (String) details.get("locale");
-                if (locale.contains("-")) {
-                    String langKey = locale.substring(0, locale.indexOf("-"));
-                    user.setLangKey(langKey);
-                } else if (locale.contains("_")) {
-                    String langKey = locale.substring(0, locale.indexOf("_"));
-                    user.setLangKey(langKey);
-                }
-            }
-            if (details.get("picture") != null) {
-                user.setImageUrl((String) details.get("picture"));
-            }
+        }
+        if (details.get("picture") != null) {
+            user.setImageUrl((String) details.get("picture"));
         }
         user.setActivated(true);
         return user;
+    }
+
+    private static Set<Authority> extractAuthorities(List<String> values) {
+        return authoritiesFromStringStream(
+            values.stream().filter(role -> role.startsWith("ROLE_"))
+        );
+    }
+
+    private static Set<Authority> authoritiesFromStringStream(Stream<String> strings) {
+        return strings
+            .map(string -> {
+                Authority auth = new Authority();
+                auth.setName(string);
+                return auth;
+            }).collect(Collectors.toSet());
     }
 }
